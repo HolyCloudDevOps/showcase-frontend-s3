afd1a075a082c0732be6fe73206cb875a3eaa65b
HolyCloudDevOps <hollydevops@gmail.com>
2024-12-29 17:25:28 -0500

    Edit tf

--- infra/main.tf
+++ infra/main.tf
@@ -8,15 +8,37 @@ terraform {
 }
 
 provider "aws" {
-  region = us-east-1
+  region = "us-east-1"
 }
 
 
-resource "aws_s3_bucket" "testbucket" {
-  bucket = "agsdgdsgsdjrsrgvsdgc"
-  acl    = "private"
+resource "aws_s3_bucket" "frontend_hosting" {
+  bucket = "shfdzscgdsvfedasdfbahebf"
+  acl    = "public-read"
+
+  website {
+    index_document = "index.html"
+    error_document = "404.html"
+  }
 
   tags = {
-    Name = "test"
+    Environment = "test"
+    Project     = "test"
   }
+}
+
+resource "aws_s3_bucket_policy" "frontend_policy" {
+  bucket = aws_s3_bucket.frontend_hosting.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action    = "s3:GetObject"
+        Effect    = "Allow"
+        Principal = "*"
+        Resource  = "${aws_s3_bucket.frontend_hosting.arn}/*"
+      }
+    ]
+  })
 }
