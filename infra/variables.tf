b3d66747dce4228e9d533ce21c94ce921b36a97f
HolyCloudDevOps <hollydevops@gmail.com>
2024-12-29 17:22:45 -0500

    Edit-new

--- infra/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "env" {
-  description = "The environment for the Terraform state (dev, staging, prod)"
-  default     = "dev"
-}
-
-variable "region" {
-  description = "AWS region"
-  default = "us-east-1"
-}
